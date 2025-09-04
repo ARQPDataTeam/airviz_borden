33ca05c1d756423d36c9808fde33562610baeeb6
bernski <bernski>
2025-09-04 08:52:17 -0400

    fixed status query for csat

--- assets/sql_queries/status_indicator.sql
+++ assets/sql_queries/status_indicator.sql
@@ -1,9 +1,9 @@
 SET TIME ZONE 'GMT';
-SELECT MAX(datetime) AS last_datetime, 'CR3000' AS source FROM bor__cr3000_v0
+SELECT MAX(datetime) AS last_datetime, 'CR3000' AS source FROM bor__cr3000_m_v0
 UNION ALL
 SELECT MAX(datetime), 'CR23X Temp' FROM bor__cr23x_m_v0
 UNION ALL
-SELECT MAX(datetime), 'CSAT' FROM bor__csat_m_v0
+SELECT MAX(datetime), 'CSAT' FROM bor__csat_v0
 UNION ALL
 SELECT MAX(datetime), 'Licor' FROM bor__lic7000_p_v0
 UNION ALL
